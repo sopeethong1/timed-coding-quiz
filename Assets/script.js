acd88bd24b64bdeac7ed788b819f974027e5ba30
Sopee Thong <tsopee@gmail.com>
2021-03-24 17:44:13 -0400

    adding highscore and start event

--- Assets/script.js
+++ Assets/script.js
@@ -1,41 +1,65 @@
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// Then I am told "Correct" or "Wrong"---ST
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and my score
+var timeEl = document.getElementById("timer");
+var startButtonEl = document.getElementById("start-button");
+var highScoresEL = document.getElementById("high-score"); 
 
-var questions = [
+function startTime() {
+ countDown = setInterval(function() {
+  if(timeLeft > 1) {
+
+    timerEl.textContent = timeLeft + "seconds remaining";
+
+    timeLeft--;
+  } else if (timeLeft === 1) {
+
+    timerEl.textContent = timeLeft + "seconds remaining";
+    timeLeft--;
+  } else {
+
+    timerEl.textContent = "";
+
+    clearInterval(timeInterval);
+    
+    displayMessage();
+  }
+} , 1000) ;
+}
+
+function startGame() {
+timeEl = 60;
+}
+
+
+ 
+
+
+var theQuiz = [
   {
-    question: "1. Choose the option that is NOT a Javascript Data Type:",
-    choices: ["Number", "Object", "String", "Booyah"],
+    question: "Choose the option that is NOT a Javascript Data Type:",
+    options: ["Number", "Object", "String", "Booyah"],
     answer: 4,
   },
 
   {
     question:
-      "2. A Javascript ____________ is a block of code designed to perform a particular task:",
-    choices: ["Argument", "Initialization", "Function", "Agreement"],
+      "Choose True or False for the following statement: A Javascript String can not use double quotes.",
+    options: ["True", "False"],
+    answer: 2,
+  },
+
+  {
+    question:
+      "A Javascript ____________ is a block of code designed to perform a particular task:",
+    options: ["Argument", "Initialization", "Function", "Agreement"],
     answer: 3,
   },
 
   {
     question:
       "Choose True or False for the following statement: Objects are variables too.",
-    choices: ["True", "False"],
+    options: ["True", "False"],
     answer: 1,
   },
-
-  {
-    question:
-      "Choose True or False for the following statement: A Javascript String can not use double quotes.",
-    choices: ["True", "False"],
-    answer: 2,
-  },
 ];
+
+  
+startButtonEl.addEventListener("click", startGame); 
