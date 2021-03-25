bf7a5687426019320de8d0f6d0a93d6ce8a86fe5
Sopee Thong <tsopee@gmail.com>
2021-03-24 21:56:29 -0400

    fixed timer

--- Assets/script.js
+++ Assets/script.js
@@ -3,52 +3,18 @@ var startButtonEl = document.getElementById("start-button");
 var highScoresEL = document.getElementById("high-score"); 
 var mainE1 = document.getElementById("main");
 
-console.log(mainE1);
+
+var count = 40;
+var interval = setInterval(function(){
+  document.getElementById('countdown').innerHTML= "Time Remaining: " + count;
+  count--;
+  if (count === 0){
+    clearInterval(interval);
+    document.getElementById('countdown').innerHTML='Times Up!';
+  }
+}, 1000);
 
 
-
-// var button = "startGame"
-// startButtonEl.addEventListener("click", function() {
-//   // If mode is dark, apply light background
-//   if (button === "startGame") {
-//     mode = "light";
-//     main.setAttribute("class", "light");
-//   }
-//   // If mode is light, apply dark background 
-//   else {
-//     mode = "dark";
-//     main.setAttribute("class", "dark");
-//   }
-// });
-
-
-
-
-function countdown() {
-  var timeLeft = 5;
-
-  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-  var timeInterval = setInterval(function () {
-    // As long as the `timeLeft` is greater than 1
-    if (timeLeft > 1) {
-      // Set the `textContent` of `timerEl` to show the remaining seconds
-      timerEl.textContent = timeLeft + ' seconds remaining';
-      // Decrement `timeLeft` by 1
-      timeLeft--;
-    } else if (timeLeft === 1) {
-      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-      timerEl.textContent = timeLeft + ' second remaining';
-      timeLeft--;
-    } else {
-      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-      timerEl.textContent = '';
-      // Use `clearInterval()` to stop the timer
-      clearInterval(timeInterval);
-      // Call the `displayMessage()` function
-      displayMessage();
-    }
-  }, 1000);
-}
 
 
  
